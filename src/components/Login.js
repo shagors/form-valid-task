dc13457f34becbea3075c795b3bed96dd387ccb3
thtshajjad <shagors@users.noreply.github.com>
2023-06-24 09:43:45 +0600

    design login and signup page with bootstrap and set routes

---
+++ src/components/Login.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [values, setValues] = useState({});
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-info vh-100">
+      <div className="bg-white p-3 rounded w-50">
+        <h2 className="text-center">Log In</h2>
+        <form action="">
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              required
+              placeholder="Enter Your Email"
+              className="form-control rounded-0"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              required
+              placeholder="Enter Your Password"
+              className="form-control rounded-0"
+            />
+          </div>
+          <button className="btn btn-success w-100">Log in</button>
+          <p className="text-center mt-3">
+            Are you agree to our terms and conditions
+          </p>
+          <p className="text-center">
+            Are you new ?
+            <Link to="/signup" className="">
+              Create an Account
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
