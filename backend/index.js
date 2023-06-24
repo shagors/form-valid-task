feb5277bba1f8ef825cb721e4971b916980ac2b7
thtshajjad <shagors@users.noreply.github.com>
2023-06-24 14:18:21 +0600

    api design for frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -10,17 +10,33 @@ const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "form_validataion",
+  database: "form",
 });
 
+// data make to server api
 app.post("/signup", (req, res) => {
-  const sql = "INSERT INTO login (`name`, `email`, `password`) VALUES (?)";
+  const sql = "INSERT INTO signup (`name`, `email`, `password`) VALUES (?)";
   const values = [req.body.name, req.body.email, req.body.password];
   db.query(sql, [values], (error, data) => {
     if (error) {
       return res.json("Error");
     }
     return res.json(data);
+  });
+});
+
+// data send to frontend api
+app.get("/login", (req, res) => {
+  const sql = "SELECT * FROM signup WHERE `email` = ? AND `password` = ?";
+  db.query(sql, [req.body.email, req.body.password], (error, data) => {
+    if (error) {
+      return res.json("Error");
+    }
+    if (data.length > 0) {
+      return res.json("Success");
+    } else {
+      return res.json("Failed");
+    }
   });
 });
 
