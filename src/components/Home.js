8d1aedf6d02ba6da3576b073d9e5787930e66aaf
thtshajjad <shagors@users.noreply.github.com>
2023-06-25 09:40:55 +0600

    table addd and data show in display... api send data and get data frontend

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/home")
+      .then((res) => setData(res.data))
+      .catch((error) => console.log(error));
+  }, []);
+  console.log(data);
   return (
-    <div className="text-center mt-4">
-      <h1 className="mb-4">Home</h1>
+    <div className="vh-100 bg-info">
+      <h1 className="text-center text-white fs-1 pt-5 fw-bold">
+        List Of Users
+      </h1>
+      <div className="d-flex vh-100 justify-content-center align-items-center">
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.map((user, index) => {
+              return (
+                <tr key={index}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
