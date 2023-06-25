2534ad95149d108170af5f79b166186a3b04a863
thtshajjad <shagors@users.noreply.github.com>
2023-06-25 11:56:17 +0600

    update api set and design it and make.. it works properly and data updated

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ app.use(json());
 app.use(cors());
 
 // jwt middleware make for secure
+
 const verifyJwt = (req, res, next) => {
   const token = req.headers["access-token"];
   if (!token) {
@@ -25,6 +26,7 @@ const verifyJwt = (req, res, next) => {
 };
 
 // create db connection with mysql
+
 const db = createConnection({
   host: "localhost",
   user: "root",
@@ -33,6 +35,7 @@ const db = createConnection({
 });
 
 // data make to server api
+
 app.post("/signup", (req, res) => {
   const sql = "INSERT INTO login (`name`, `email`, `password`) VALUES (?)";
   const values = [req.body.name, req.body.email, req.body.password];
@@ -45,11 +48,13 @@ app.post("/signup", (req, res) => {
 });
 
 // check jwt token pass to local storage
+
 app.get("/checkauth", verifyJwt, (req, res) => {
   return res.json("Authen");
 });
 
 // data send to frontend api
+
 app.post("/login", (req, res) => {
   const sql = "SELECT * FROM login WHERE `email` = ? AND `password` = ?";
   db.query(sql, [req.body.email, req.body.password], (error, data) => {
@@ -69,6 +74,7 @@ app.post("/login", (req, res) => {
 });
 
 // data send to frontend and show data api
+
 app.get("/home", (req, res) => {
   const sql = "SELECT * FROM login";
   db.query(sql, (error, result) => {
@@ -78,17 +84,28 @@ app.get("/home", (req, res) => {
 });
 
 // single id read and show api
+
 app.get("/read/:id", (req, res) => {
   const sql = "SELECT * FROM login WHERE id = ?";
   const id = req.params.id;
-
   db.query(sql, [id], (error, result) => {
     if (error) return res.json({ Message: "Something wrong Occured" });
     return res.json(result);
   });
 });
 
+// Update api
+
+app.put("/update/:id", (req, res) => {
+  const sql = "UPDATE login SET `name` = ? , `email` = ? WHERE id=?";
+  const id = req.params.id;
+  db.query(sql, [req.body.name, req.body.email, id], (error, result) => {
+    if (error) return res.json({ Message: "Something wrong Occured" });
+    return res.json(result);
+  });
+});
 // for test
+
 app.listen(5000, () => {
   console.log("Port is connected");
 });
